e3ea3de860f47a943ee74521a758ff807317d1e0
Harman <hsk9.nahan@gmail.com>
2025-01-21 06:57:26 +0530

    Committing fourth time

--- app.py
+++ app.py
@@ -1,8 +1,12 @@
 def addition(a,b):
     return a+b
 
+def division(a,b):
+    return a//b
+
 def main():
-    print(addition(3,4))
+    print(division(4,2))
+    # print(addition(3,4))
 
 if __name__=="__main__":
     main()
